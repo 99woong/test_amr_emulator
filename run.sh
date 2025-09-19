f96a81de0c4445a81229e891550ecd57e4f01800
99woong <99woong@gmail.com>
2025-09-19 17:52:25 +0900

    add rviz2, change excution order in run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+rviz2 &
+
+sleep 1
+
+./bin/driving_viewer ./config/amr_params.yaml &
+
+sleep 3
 export LD_LIBRARY_PATH="$(pwd)/lib:$LD_LIBRARY_PATH"
-cd ./bin
-./amr_emulator
+./bin/amr_emulator /home/zenix/test_amr_emulator/config/amr_params.yaml
+
 
 
